af1db77e75e604534dffad970afa3ce612cc4a88
Connor Beardsmore <connor.beardsmore@gmail.com>
2017-03-22 21:49:15 +0800

    START ON DES CONSTANTS

---
+++ DES/DES.java
@@ -0,0 +1,18 @@
+/***************************************************************************
+*	FILE: DES.java
+*	AUTHOR: Connor Beardsmore - 15504319
+*	UNIT: FCC200
+*	PURPOSE: Performs DES encryption or decryption on a given file
+*   LAST MOD: 21/03/17
+*   REQUIRES: NONE
+***************************************************************************/
+
+public class DES
+{
+    public static void main( String[] args )
+    {
+        //DES stuff
+    }
+
+//---------------------------------------------------------------------------
+}
