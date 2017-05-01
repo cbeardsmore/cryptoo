c3ffebb111c2a26b515950fc03d3d242c66a749f
Connor Beardsmore <connor.beardsmore@gmail.com>
2017-05-01 10:57:07 +0800

    start of rsa report format

---
+++ RSA/RSA.java
@@ -0,0 +1,34 @@
+/***************************************************************************
+*	FILE: RSA.java
+*	AUTHOR: Connor Beardsmore - 15504319
+*	UNIT: FCC200
+*	PURPOSE: Performs RSA public-key encryption or decryption on a given file
+*   LAST MOD: 01/05/17
+*   REQUIRES: NONE
+***************************************************************************/
+
+import java.util.*;
+import java.io.*;
+
+public class RSA
+{
+//---------------------------------------------------------------------------
+
+    public static void main( String[] args )
+    {
+
+    }
+
+//---------------------------------------------------------------------------
+    //NAME:
+    //IMPORT:
+    //EXPORT:
+    //PURPOSE:
+
+    public static int modularExpo( int a, int b, int n )
+    {
+        return 0;
+    }
+
+//---------------------------------------------------------------------------
+}
