611cc4407ab36cf9e122a16da74a147ba9630c62
Connor Beardsmore <connor.beardsmore@gmail.com>
2017-05-02 17:25:51 +0800

    birthday attack complete

--- RSA/RSA.java
+++ RSA/RSA.java
@@ -12,22 +12,58 @@ import java.io.*;
 
 public class RSA
 {
+    public static final long LIMIT = 10000000000L;
 //---------------------------------------------------------------------------
 
     public static void main( String[] args )
     {
+        int base = 236;
+        int exponent = 239721;
+        int modulus = 2491;
 
+        System.out.println( "BASE:     " + base );
+        System.out.println( "EXPONENT: " + exponent );
+        System.out.println( "MODULUS:  " + modulus );
+
+        int result = modularExpo( base, exponent, modulus );
+        System.out.println( "RESULT:   " + result );
     }
 
 //---------------------------------------------------------------------------
-    //NAME:
-    //IMPORT:
-    //EXPORT:
-    //PURPOSE:
+    //NAME: modularExpo()
+    //IMPORT: base (int), exponent (int), modulus (int)
+    //EXPORT: result (int)
+    //PURPOSE: Calculate the value base^exponent mod modulus efficiently
 
-    public static int modularExpo( int a, int b, int n )
+    public static int modularExpo( int base, int exponent, int modulus )
     {
-        return 0;
+        int result = 1;
+
+        //check upper limit
+        if ( ( base > LIMIT ) || ( exponent > LIMIT ) || ( modulus > LIMIT) )
+            throw new IllegalArgumentException("INVALID MODULAR EXPO NUMBER");
+            
+        //anything mod 1 results in 0
+        if ( modulus == 1 )
+            return 0;
+
+        //reduce base to the lowest form
+        base = base % modulus;
+
+        //loop until all exponents reviewed
+        while ( exponent > 0 )
+        {
+            //if the bit is set (from lowest to highest order bit)
+            if ( ( exponent & 1 ) == 1 )
+                //increase result by the base
+                result = ( result * base ) % modulus;
+            //shift exponent to consider the next higher order bit
+            exponent = exponent >> 1;
+            //increase the base
+            base = ( base * base ) % modulus;
+        }
+
+        return result;
     }
 
 //---------------------------------------------------------------------------
