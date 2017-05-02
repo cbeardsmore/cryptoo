50254973a78529fc2f848efadd1668d3b3bd2ba2
Connor Beardsmore <connor.beardsmore@gmail.com>
2017-05-02 18:13:49 +0800

    copy across C functions, need to port to Java

--- RSA/NumberTheory.java
+++ RSA/NumberTheory.java
@@ -7,6 +7,8 @@
 *   REQUIRES: NONE
 ***************************************************************************/
 
+import java.util.Random;
+
 public class NumberTheory
 {
     //CONSTANTS
@@ -49,6 +51,98 @@ public class NumberTheory
         }
 
         return result;
+    }
+
+//------------------------------------------------------------------------------
+// FUNCTION: primalityTest
+// IMPORT: p (int), tests (int)
+// PURPOSE: Determine if a number is prime or not to some confidence level
+
+    public static boolean primalityTest( int prime, int tests )
+    {
+        long a, r;
+        long expo;
+
+        Random rand = new Random();
+
+        // PERFORM MULTIPLE TESTS
+        for ( int ii = 0; ii < tests; ii++ )
+        {
+            // CALCULATE r RESULT
+            a = rand.next() % ( prime - 1 ) + 1;
+            expo = ( prime - 1 ) >> 1;
+            r = (long)pow( a, expo ) % prime;
+
+            // IF r IS NOT 1 OR -1 IT IS 100% NOT PRIME
+            if ( ( r != 1 ) && ( r != -1 ) )
+                if ( ( r != ( prime - 1 ) ) && ( r != ( prime - 1 ) ) )
+                return false;
+        }
+
+        return true;
+    }
+
+//------------------------------------------------------------------------------
+// FUNCTION: gcd
+// IMPORT: a (int), b (int)
+// PURPOSE: Find greatest common denominator of 2 numbers
+
+    int gcdFunction( int a, int b )
+    {
+        int gcd = 1;
+        int quotient;
+        int residue;
+
+        if ( a < b )
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // CHECK IF EITHER NUMBER IS 0
+        if ( a == 0 )   return b;
+        if ( b == 0 )   return a;
+
+        // SATISFY THE EQUATION: A = B * quotient + residue
+        quotient = a / b;
+        residue = a - ( b * quotient );
+
+        // RECURSIVELY CALL GCD
+        gcd = gcdFunction( b, residue );
+
+        return gcd;
+    }
+
+//------------------------------------------------------------------------------
+// FUNCTION: extendEuclid
+// IMPORT: a (int), n (int)
+// PURPOSE: Extended Euclidean algorithm to find inverse modular
+
+    int extendEuclid( int a, int n )
+    {
+        int t = 0, newt = 1;
+        int r = n, newr = a;
+        int q = 0, temp = 0;
+
+        if ( gcdFunction( a, n ) != 1 )
+            return -1;
+
+        while ( newr != 0 )
+        {
+            q = r / newr;
+            temp = t;
+            t = newt;
+            newt = temp - ( q * newt );
+            temp = r;
+            r = newr;
+            newr = temp - ( q * newr );
+        }
+
+        if ( t < 0 )
+            t = t + n;
+
+        return t;
     }
 
 //---------------------------------------------------------------------------
